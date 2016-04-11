107deea2ab8c7f52b53be70ca23e6df2f6d9ece4
root <root@hatchery.new>
2016-04-12 00:52:04 +0400

    Adding base files for Docker image

---
+++ app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from flask import Flask
+import socket
+app = Flask(__name__)
+
+ip_address = socket.gethostbyname(socket.gethostname())
+docker_hostname = socket.gethostname()
+
+@app.route('/')
+def index():
+    return 'Flask Dockerized! \n' + 'IPv4 address: \n' + str(ip_address) + '\n' + 'Container hostname: \n \t' + str(docker_hostname)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(8350))
