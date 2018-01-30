663ff2f6386c1d9ba7e00b7a47f104ba0015628b
Stephen Knight <stephen.knight2@emc.com>
2018-01-30 10:17:44 +0400

    updating version output

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ docker_hostname = socket.gethostname()
 
 @app.route('/')
 def index():
-    return 'Flask Dockerized! \n' + 'IPv4 address: \n' + str(ip_address) + '\n' + 'Container hostname: \n \t' + str(docker_hostname)
+    return 'Python Flask Dockerized! \n' + 'Version: v2 \n' + 'IPv4 address: \n' + str(ip_address) + '\n' + 'Container hostname: \n \t' + str(docker_hostname)
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(8350))
